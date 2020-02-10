b6852a9191b831e118a25360dbb35bcd5a0c3242
Cristian Pintado <cpintado17@gmail.com>
2020-02-10 20:02:32 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -4,7 +4,8 @@ while levitation_force < 10
   using_while = "Wingardium Leviosa"
   puts "Wingardium Leviosa. #{levitation_force}"
   levitation_force = levitation_force + 1
-  
+  using_while
+  end
 end
 
 using_while 
